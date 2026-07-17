aa57f5b4b497a05c9b35e31d9b60c3594f1463f8
medzenter-svg <medzenter@icloud.com>
2026-07-17 19:48:18 +0200

    Add files via upload

--- store-enhance.js
+++ store-enhance.js
@@ -1,4 +1,10 @@
 (()=>{
+  const header=document.querySelector('header');
+  if(header){
+    const brandLeaf=[...header.querySelectorAll('*')].find(el=>el.children.length===0&&/handycover\.com/i.test(el.textContent||''));
+    if(brandLeaf)brandLeaf.textContent=brandLeaf.textContent.replace(/handycover\.com/i,'HandyCover.Store');
+  }
+
   document.querySelectorAll('a[href*="amazon.de"]').forEach(a=>{
     a.setAttribute('rel','sponsored nofollow noopener');
     a.setAttribute('target','_blank');
